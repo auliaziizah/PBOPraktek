2b3e5ab59854468551315694d50ae8be3a0cb742
Aulia Aziizah Fauziyyah <117973604+auliaziizah@users.noreply.github.com>
2023-09-17 22:59:23 +0700

    Add files via upload

---
+++ Pertemuan 4/praktikum4/src/praktikum4/KelasSatu.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum4;
+
+/**
+ *
+ * @author Asus
+ */
+public class KelasSatu {
+    { //inisialisasi non-static
+    System.out.println(11);
+    }
+    static //inisialisasi static
+    {
+    System.out.println(2);
+    }
+    public KelasSatu(int i)
+    {
+    System.out.println(3);
+    }
+    public KelasSatu()
+    {
+    System.out.println(4);
+    }
+}
