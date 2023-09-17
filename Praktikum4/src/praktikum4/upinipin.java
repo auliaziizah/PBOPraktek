ab97bb884c027b7322653d676bfcb242cc25168c
Aulia Aziizah Fauziyyah <117973604+auliaziizah@users.noreply.github.com>
2023-09-17 23:15:55 +0700

    Add files via upload

---
+++ Praktikum4/src/praktikum4/upinipin.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum4;
+
+/**
+ *
+ * @author Asus
+ */
+public class upinipin {
+    public static void main(String[] args) { 
+        item name = new item("upin");
+    }
+}
